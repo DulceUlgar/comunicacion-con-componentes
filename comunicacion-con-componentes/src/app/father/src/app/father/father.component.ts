b28b3fce3079c643f592cdb8e3e9d3ed9bc24267
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-07-04 21:24:22 +0200

    father ts

---
+++ comunicacion-con-componentes/src/app/father/src/app/father/father.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-father',
+  templateUrl: './father.component.html',
+  styleUrls: ['./father.component.scss'],
+})
+export class FatherComponent {
+  employees: any[] = [];
+  features: { [employeeId: string]: string[] } = {};
+
+  addEmployee(employee: any) {
+    this.employees.push(employee);
+    this.features[employee.id] = [];
+  }
+  addFeature(employee: any) {
+    const feature = 'Nueva característica';
+    this.features[employee.id].push(feature);
+  }
+  getFeatures(employeeId: string): string[] {
+    return this.features[employeeId] || [];
+  }
+}
