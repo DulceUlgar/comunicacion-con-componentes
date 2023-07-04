6b01e8de3ec9a3578a0271033c58734129254530
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-07-04 21:39:21 +0200

    son ts

---
+++ comunicacion-con-componentes/src/app/son/son.component.ts
@@ -0,0 +1,37 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-son',
+  templateUrl: './son.component.html',
+  styleUrls: ['./son.component.scss'],
+})
+export class SonComponent {
+  id: string = '';
+  name: string = '';
+  lastName: string = '';
+  position: string = '';
+  salary: number = 0;
+
+  @Output() employeeAdded = new EventEmitter<any>();
+
+  submitForm() {
+    const employee = {
+      id: this.id,
+      name: this.name,
+      lastName: this.lastName,
+      position: this.position,
+      salary: this.salary,
+    };
+
+    this.employeeAdded.emit(employee);
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.id = '';
+    this.name = '';
+    this.lastName = '';
+    this.position = '';
+    this.salary = 0;
+  }
+}
